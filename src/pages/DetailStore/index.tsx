55d68fd6d1082729531ae1eaa9fadf1ed5ccd7a3
Kássia Catarine <kassia_catarine.15@hotmail.com>
2020-06-12 15:40:01 -0300

    Adicionando componente de detalhes da loja
    Signed-off-by: Kássia Catarine <kassia_catarine.15@hotmail.com>

---
+++ src/pages/DetailStore/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './styles';
+
+const DetailStore = () => {
+  return (
+    <Container>
+      <h1>Olas</h1>
+    </Container>
+  );
+};
+
+export default DetailStore;
--- src/routes.tsx
+++ src/routes.tsx
@@ -3,14 +3,16 @@ import { Route, BrowserRouter } from 'react-router-dom';
 
 import ListStores from './pages/ListStores';
 import CreateStore from './pages/CreateStore';
+import DetailStore from './pages/DetailStore';
 
 const Routes = () => {
   return (
     <BrowserRouter>
-      <Route component={ListStores} path="/" />
-      <Route component={CreateStore} path="/create-store" />
+      <Route component={ListStores} path='/' exact />
+      <Route component={CreateStore} path='/create-store' />
+      <Route component={DetailStore} path='/store' />
     </BrowserRouter>
   );
-}
+};
 
 export default Routes;
