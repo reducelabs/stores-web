a9e7ea9000edf104839be4e5d9fd00cc43557cab
Kássia Catarine <kassia_catarine.15@hotmail.com>
2020-06-15 23:25:12 -0300

    Informaçoes da página home
    Signed-off-by: Kássia Catarine <kassia_catarine.15@hotmail.com>

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,49 @@
 import React from 'react';
-import { Container, Header } from './styles';
+import {
+  Container,
+  Header,
+  Logo,
+  Title,
+  SubTitle,
+  Content,
+  LinkRegister,
+  TextButton,
+  IconButton,
+  TitleHighlight,
+} from './styles';
+import { IconContext } from 'react-icons';
+import { FaSignInAlt } from 'react-icons/fa';
 
 const Home = () => {
   return (
     <Container>
-      <Header></Header>
+      <Header>
+        <Logo>Peek</Logo>
+      </Header>
+      <Content>
+        <Title>
+          O seu <TitleHighlight>negócio local</TitleHighlight> acessível de
+          novas formas
+        </Title>
+        <SubTitle>
+          Estar diponível para os clientes presencialmente e online nunca foi
+          tão fácil, amplie o seu negócio com a localizando da loja e exposição
+          dos produtos.
+        </SubTitle>
+        <LinkRegister to='loja/cadastrar'>
+          <IconContext.Provider
+            value={{
+              color: 'white',
+              size: '20px',
+            }}
+          >
+            <IconButton>
+              <FaSignInAlt />
+            </IconButton>
+          </IconContext.Provider>
+          <TextButton>Cadastre a sua loja</TextButton>
+        </LinkRegister>
+      </Content>
     </Container>
   );
 };
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,9 +1,78 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const Container = styled.div`
   display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
 `;
 
 export const Header = styled.header`
   display: flex;
+  height: 10vh;
+`;
+
+export const Logo = styled.div`
+  color: ${(props) => props.theme.colors.primary};
+  font-size: 32px;
+  font-weight: 700;
+  margin: 10px 58px;
+`;
+
+export const Title = styled.h1`
+  font-style: normal;
+  font-weight: bold;
+  font-size: 54px;
+  line-height: 64px;
+  margin-bottom: 24px;
+`;
+
+export const TitleHighlight = styled.span`
+  color: ${(props) => props.theme.colors.primary};
+`;
+
+export const SubTitle = styled.p`
+  font-size: 24px;
+  line-height: 38px;
+`;
+
+export const Content = styled.main`
+  width: 672px;
+  margin: 200px 250px;
+  display: flex;
+  flex-direction: column;
+  justify-self: center;
+`;
+
+export const LinkRegister = styled(Link)`
+  width: 100%;
+  max-width: 360px;
+  height: 58px;
+  background: ${(props) => props.theme.colors.primary};
+  border-radius: 8px;
+  border: 0;
+  text-decoration: none;
+
+  display: flex;
+  align-items: center;
+  overflow: hidden;
+  margin-top: 40px;
+`;
+
+export const IconButton = styled.span`
+  display: block;
+  background: rgba(0, 0, 0, 0.08);
+  width: 58px;
+  height: 58px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: background-color 0.2;
+`;
+
+export const TextButton = styled.strong`
+  flex: 1;
+  text-align: center;
+  color: #fff;
 `;
